1699f202bff7117088823b4aa0c599917b42b806
sxmurxy2007 <pashashaxmatov2005@mail.ru>
2023-06-22 17:22:06 +1000

    init

---
+++ src/main/java/dev/sxmurxy/template/TemplateMod.java
@@ -0,0 +1,23 @@
+package dev.sxmurxy.template;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TemplateMod implements ModInitializer {
+
+	public static final String MOD_ID = "template";
+	private static TemplateMod instance;
+
+	public TemplateMod() {
+		instance = this;
+	}
+
+	@Override
+	public void onInitialize() {
+
+	}
+
+	public static TemplateMod getInstance() {
+		return instance;
+	}
+
+}
